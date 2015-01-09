4cbc8791a1e63a6275a52e1881e3cd74e069238d
Matt Perry <mpcomplete@chromium.org>
2015-01-08 21:22:42 -0600

    initial commit

---
+++ app/src/main/java/com/example/android/opengl/MyGLRenderer.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.opengl;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private static final String TAG = "MyGLRenderer";
+    private static int mProgram = -1;
+
+    private static final String vertexShaderCode =
+        "attribute vec4 vPosition;" +
+        "void main() {" +
+        "  gl_Position = vPosition;" +
+        "}";
+
+    private static final String fragmentShaderCode =
+        "precision mediump float;" +
+        "uniform vec4 vColor;" +
+        "void main() {" +
+        "  gl_FragColor = vColor;" +
+        "}";
+
+    static final int COORDS_PER_VERTEX = 3;
+    static final int VERTEX_STRIDE = COORDS_PER_VERTEX * 4;
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawOrderBuffer;
+    private int drawOrderLength;
+
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mProgram = loadProgram(vertexShaderCode, fragmentShaderCode);
+
+        final float squareCoords[] = {
+            -1.0f,  -1.0f, 0.0f,
+            -1.0f,   1.0f, 0.0f,
+             1.0f,   1.0f, 0.0f,
+             1.0f,  -1.0f, 0.0f,
+        };
+        final short drawOrder[] = {0, 1, 2, 0, 2, 3};
+        vertexBuffer = createBuffer(squareCoords);
+        drawOrderBuffer = createBuffer(drawOrder);
+        drawOrderLength = drawOrder.length;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        GLES20.glUseProgram(mProgram);
+
+        final float color[] = {0.2f, 0.709803922f, 0.898039216f, 1.0f};
+        int colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+        drawVertexBuffer(vertexBuffer, drawOrderBuffer, drawOrderLength);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    public static int loadShader(int type, String shaderCode) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, shaderCode);
+        checkGlError("glShader");
+        GLES20.glCompileShader(shader);
+        checkGlError("glShader2");
+        return shader;
+    }
+
+    public static int loadProgram(String vertexShaderCode, String fragmentShaderCode) {
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        int program = GLES20.glCreateProgram();
+        GLES20.glAttachShader(program, vertexShader);
+        GLES20.glAttachShader(program, fragmentShader);
+        GLES20.glLinkProgram(program);
+
+        return program;
+    }
+
+    public static FloatBuffer createBuffer(float[] array) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(array.length * 4);  // 4 bytes per float
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer buffer = bb.asFloatBuffer();
+        buffer.put(array);
+        buffer.position(0);
+        return buffer;
+    }
+
+    public static ShortBuffer createBuffer(short[] array) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(array.length * 2);  // 2 bytes per short
+        bb.order(ByteOrder.nativeOrder());
+        ShortBuffer buffer = bb.asShortBuffer();
+        buffer.put(array);
+        buffer.position(0);
+        return buffer;
+    }
+
+    public static void drawVertexBuffer(FloatBuffer vertexBuffer,
+                                        ShortBuffer drawOrderBuffer, int drawOrderLength) {
+        int positionHandle  = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(
+            positionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, VERTEX_STRIDE, vertexBuffer);
+        GLES20.glDrawElements(
+            GLES20.GL_TRIANGLES, drawOrderLength,
+            GLES20.GL_UNSIGNED_SHORT, drawOrderBuffer);
+        GLES20.glDisableVertexAttribArray(positionHandle);
+
+    }
+
+    /**
+    * Utility method for debugging OpenGL calls. Provide the name of the call
+    * just after making it:
+    *
+    * <pre>
+    * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+    * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
+    *
+    * If the operation is not successful, the check throws an error.
+    *
+    * @param glOperation - Name of the OpenGL call to check.
+    */
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
+
+}
